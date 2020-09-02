1e79178dc421309c8c23f40aa66adb5d51cda61d
daniel.ach <MrAch26@users.noreply.github.com>
2020-09-03 00:04:34 +0300

    xp

---
+++ index.py
@@ -0,0 +1,66 @@
+# EXE 1
+# print("Hello world \n"*4)
+
+# EXE 2
+# (99^3) * 8 -- 768
+
+# EXE 3
+# name = "Daniel"
+# age = 21
+# shoe_size = 42.5
+# info = (f"My name is {name} I am {age} and my shoe size is {shoe_size}")
+# print(info)
+
+# EXE 4
+# computer_brand = "Apple"
+# print(f"I have a {computer_brand} computer")
+
+# EXE 5 
+# 5<3 #False 
+# 3==3 # true
+# 3 == "3" # false 
+# "3" > 3 #error
+# "Hello" == "hello" #false
+
+# EXE 6
+# height = int(input("Whats your height ? in inches"))
+# if height>=35:
+#     print("you can ride")
+# else:
+#     print("You cannot sorry... Eat more soup")
+
+# EXE 7 
+# num = int(input("Enter a Num."))
+
+# if num % 2 == 0 :
+#     print("This is an EVEN number (PAIR en francais)")
+
+# else :
+#     print("This is an ODD number (Impair en francais)")
+
+
+# EXE - XP GOLD 
+# EXE 1 - ok
+
+# EXE 2 - 
+# PATH="/Library/Frameworks/Python.framework/Versions/3.6/bin:${PATH}"
+# export PATH
+# alias python='python3'
+# alias 3='python3'
+# alias py='python3'
+
+# EXE3 
+# print((("Hello world \n")*4)+("I love python \n")*4)
+
+# EXE 4 
+# x = int(input("Enter a Num."))
+# print(x+x*x+x*x*x+x*x*x*x)
+
+# number_x = int(input("Enter a Num."))
+# new_number_x = 0
+# for iteration in range(4):
+#     new_number_x += number_x * iteration
+# print(new_number_x)
+
+# EXE 5
+
